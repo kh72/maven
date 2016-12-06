da5418b246d1822a8eeb7217f1687ee3038aeaf7
EC2 Default User <ec2-user@ip-172-31-33-31.eu-west-1.compute.internal>
2016-12-06 13:13:15 +0000

    Modified App.java

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World One More Time!" );
+        System.out.println( "Hello World For the Third Time!" );
     }
 }
